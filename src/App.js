59ab13c6423a64a7d73860cf5a125cd4079d4bc0
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-06-22 12:58:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact';
 import './App.css';
+
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       < Contact 
+        name="Ian Ryan" 
+        avatar="https://randomuser.me/api/portraits/men/83.jpg"
+        online="true"
+       />
+       < Contact 
+        name="Marjorie Little" 
+        avatar="https://randomuser.me/api/portraits/women/50.jpg"
+        online="false"
+       />
+       < Contact 
+        name="Cathy Lucas" 
+        avatar="https://randomuser.me/api/portraits/women/8.jpg"
+        online="true"
+       />
     </div>
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../App.css';
+import './style.css';
+import PropTypes from 'prop-types';
+
+
+function Contact (props){
+    return(
+        <div className="Contact">
+            <img className="avatar" src={props.avatar} alt={props.name}/>
+            <div>     
+                <h4 className="name">{props.name}</h4>
+                <div className="status">
+                    <div className= { props.online === "true" ? "status-online" : "status-offline" }></div>
+                    <div className="status-text">{props.online === "true" ? "online" : "offline"}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+Contact.propTypes = {
+    avatar: PropTypes.string.isRequired, 
+    name: PropTypes.string.isRequired, 
+    online: PropTypes.bool.isRequired,
+};
+
+export default Contact ;
+
