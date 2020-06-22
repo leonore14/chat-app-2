6c08a233d4847aa31c2f3b8b29dcb957904d8863
leonore <63911892+leonore14@users.noreply.github.com>
2020-06-22 19:51:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import './App.css';
-import ContactList from './components/ContactList';
-
+import React from "react";
+import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <ContactList />
-    </div>
+      <div className="App">
+        <ContactList />
+      </div>
     </>
   );
 }
 
 export default App;
-
-
