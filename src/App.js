d5e0b1e9caab6d8e35b9a2cadc53bb3024cf3512
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-06-22 15:05:29 +0200

    react quest 3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import Contact from './components/Contact';
 import './App.css';
+import ContactList from './components/ContactList';
 
 
 function App() {
   return (
     <>
     <div className="App">
-       < Contact 
-        name="Ian Ryan" 
-        avatar="https://randomuser.me/api/portraits/men/83.jpg"
-        online
-       />
-       < Contact 
-        name="Marjorie Little" 
-        avatar="https://randomuser.me/api/portraits/women/50.jpg"
-        
-       />
-       < Contact 
-        name="Cathy Lucas" 
-        avatar="https://randomuser.me/api/portraits/women/8.jpg"
-        online
-       />
+      <ContactList />
     </div>
     </>
   );
