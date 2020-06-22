128f4719f75c4a44d225b3833b0f68f7c2de8c34
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-06-22 19:48:35 +0200

    last update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import './style.css';
 
@@ -17,15 +17,15 @@ class Contact extends React.Component{
             <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
             <div>     
                 <h4 className="name">{this.props.name}</h4>
-                <div className="status">
-                    <div className={this.state.online ?"status-online": "status-offline"}
-                        onClick={event => {
-                            const newOnline = !this.state.online;
-                            this.setState({ online: newOnline });
-                        }}
-                    >
+                <div className="status"
+                    onClick={event => {
+                        const newOnline = !this.state.online;
+                        this.setState({ online: newOnline });
+                    }}
+                >
+                    <div className={this.state.online ?"status-online": "status-offline"}>
                     </div>
-                    <p className="status-text">{this.props.online ? "online": "offline"}</p>
+                    <p className="status-text">{this.state.online ? "online": "offline"}</p>
                 </div>
             </div>
         </div>
